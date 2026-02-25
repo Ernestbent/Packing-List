498ddaa30e3cbc2ea3bec50252f33c1336bd7063
Ernest <ernestben69@gmail.com>
2026-02-25 10:14:41 +0300

    Pushing to Production

--- autozonepro/autozonepro/doctype/packing_list/packing_list.py
+++ autozonepro/autozonepro/doctype/packing_list/packing_list.py
@@ -5,7 +5,6 @@ class PackingList(Document):
     def before_submit(self):
         self.validate_packer()
         self.validate_camera()
-        # self.validate_packaging_verified()
 
     def validate_packer(self):
         if not self.custom_packer or self.custom_packer == 'Select':
@@ -14,24 +13,6 @@ class PackingList(Document):
     def validate_camera(self):
         if not self.custom_camera_number or self.custom_camera_number == 'Select':
             frappe.throw("Please select a Camera Number before submitting.")
-
-    # def validate_packaging_verified(self):
-    #     if not self.table_hqkk:
-    #         frappe.throw(
-    #             "Please add Packaging Details before submitting."
-    #         )
-
-    #     unverified = [
-    #         row.idx for row in self.table_hqkk
-    #         if not row.verifier_2
-    #     ]
-
-    #     if unverified:
-    #         rows = ", ".join(str(r) for r in unverified)
-    #         frappe.throw(
-    #             f"Please verify all rows before submitting.<br><br>"
-    #             f"Unverified row(s): <b>{rows}</b>"
-    #         )
 
     def on_submit(self):
         if self.custom_sales_order:
@@ -43,7 +24,6 @@ class PackingList(Document):
                     'workflow_state',
                     'Packed'
                 )
-                frappe.db.commit()
                 frappe.publish_realtime('reload_form', {
                     'doctype': 'Sales Order',
                     'name': self.custom_sales_order
