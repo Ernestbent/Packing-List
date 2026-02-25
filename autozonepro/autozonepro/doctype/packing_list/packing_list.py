91ef509e1a610a8b23b3fcfbb9346a40d223c30c
Ernest <ernestben69@gmail.com>
2026-02-25 08:45:39 +0300

    Pushing to Production

--- autozonepro/autozonepro/doctype/packing_list/packing_list.py
+++ autozonepro/autozonepro/doctype/packing_list/packing_list.py
@@ -5,7 +5,7 @@ class PackingList(Document):
     def before_submit(self):
         self.validate_packer()
         self.validate_camera()
-        self.validate_packaging_verified()
+        # self.validate_packaging_verified()
 
     def validate_packer(self):
         if not self.custom_packer or self.custom_packer == 'Select':
@@ -15,23 +15,23 @@ class PackingList(Document):
         if not self.custom_camera_number or self.custom_camera_number == 'Select':
             frappe.throw("Please select a Camera Number before submitting.")
 
-    def validate_packaging_verified(self):
-        if not self.table_hqkk:
-            frappe.throw(
-                "Please add Packaging Details before submitting."
-            )
+    # def validate_packaging_verified(self):
+    #     if not self.table_hqkk:
+    #         frappe.throw(
+    #             "Please add Packaging Details before submitting."
+    #         )
 
-        unverified = [
-            row.idx for row in self.table_hqkk
-            if not row.verifier_2
-        ]
+    #     unverified = [
+    #         row.idx for row in self.table_hqkk
+    #         if not row.verifier_2
+    #     ]
 
-        if unverified:
-            rows = ", ".join(str(r) for r in unverified)
-            frappe.throw(
-                f"Please verify all rows before submitting.<br><br>"
-                f"Unverified row(s): <b>{rows}</b>"
-            )
+    #     if unverified:
+    #         rows = ", ".join(str(r) for r in unverified)
+    #         frappe.throw(
+    #             f"Please verify all rows before submitting.<br><br>"
+    #             f"Unverified row(s): <b>{rows}</b>"
+    #         )
 
     def on_submit(self):
         if self.custom_sales_order:
--- autozonepro/public/js/packing_list_logic.js
+++ autozonepro/public/js/packing_list_logic.js
@@ -17,30 +17,25 @@ frappe.ui.form.on('Packing List', {
 
 function load_pl_items(frm) {
     frappe.db.get_doc('Pick List', frm.doc.custom_pick_list)
-    .then(function(pl) {
-        if (!pl.locations || pl.locations.length === 0) {
-            frappe.msgprint(__('This Pick List has no items. Please click "Get Item Locations" on the Pick List first.'));
-            return;
-        }
-
-        frm.clear_table('table_ttya');
-
-        pl.locations.forEach(function(loc) {
-            let row = frm.add_child('table_ttya');
-            row.item = loc.item_code;
-            row.item_name = loc.item_name;
-            row.qty = loc.picked_qty; 
-            row.uom = loc.uom;
-        });
-
-        frm.refresh_field('table_ttya');
-
-        // Set customer separately after table
-        frappe.db.get_value('Pick List', frm.doc.custom_pick_list, 'customer')
-        .then(function(r) {
-            if (r.message && r.message.customer) {
-                frm.set_value('custom_customer', r.message.customer);
+        .then(function(pl) {
+            if (!pl.locations || pl.locations.length === 0) {
+                frappe.msgprint(__('This Pick List has no items. Please click "Get Item Locations" on the Pick List first.'));
+                return;
             }
+            frm.clear_table('table_ttya');
+            pl.locations.forEach(function(loc) {
+                let row = frm.add_child('table_ttya');
+                row.item = loc.item_code;
+                row.item_name = loc.item_name;
+                row.qty = loc.picked_qty;  // ← fixed: was loc.qty
+                row.uom = loc.uom;
+            });
+            frm.refresh_field('table_ttya');
+            frappe.db.get_value('Pick List', frm.doc.custom_pick_list, 'customer')
+                .then(function(r) {
+                    if (r.message && r.message.customer) {
+                        frm.set_value('custom_customer', r.message.customer);
+                    }
+                });
         });
-    });
 }
