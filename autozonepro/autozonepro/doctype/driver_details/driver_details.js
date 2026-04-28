ac6a9c6de7e68e1ee712470d88908977fd5e5e79
Ernestbent <ernestben69@gmail.com>
2026-04-28 11:22:57 +0300

    Testing Pipelines

---
+++ autozonepro/autozonepro/doctype/driver_details/driver_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ernest Benedict and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Driver Details", {
+// 	refresh(frm) {
+
+// 	},
+// });
